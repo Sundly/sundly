2e81edf39a2d2909294b5b477155819f17643845
Ben Orozco <benoror@gmail.com>
2018-10-20 12:36:29 -0500

    Feat[react-router]: Draft protected route fakeAuth
    Source: https://tylermcginnis.com/react-router-protected-routes-authentication/

--- src/index.js
+++ src/index.js
@@ -1,19 +1,112 @@
 // Dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+// import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
 
 // Styles
 import './index.css';
 
 //Routes
-import AppRoutes from './routes'
+// import AppRoutes from './routes'
 
 import registerServiceWorker from './registerServiceWorker';
 
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100) // fake async
+  }
+}
+
+const Public = () => <h3>Public</h3>
+const Protected = () => <h3>Protected</h3>
+
+class Login extends React.Component {
+  state = {
+    redirectToReferrer: false
+  }
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState(() => ({
+        redirectToReferrer: true
+      }))
+    })
+  }
+
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
+
+    if (redirectToReferrer === true) {
+      return <Redirect to={from} />
+    }
+
+    return (
+      <div>
+        <p>You must log in to view the page</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    )
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to={{
+          pathname: '/login',
+          state: { from: props.location }
+        }} />
+  )} />
+)
+
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome! <button onClick={() => {
+        fakeAuth.signout(() => history.push('/'))
+      }}>Sign out</button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+))
+
 ReactDOM.render(
-  <BrowserRouter>
-      <AppRoutes />
-  </BrowserRouter>, 
+  <Router>
+    <div>
+      <AuthButton/>
+      <ul>
+        <li><Link to="/public">Public Page</Link></li>
+        <li><Link to="/protected">Protected Page</Link></li>
+      </ul>
+      <Route path="/public" component={Public}/>
+      <Route path="/login" component={Login}/>
+      <PrivateRoute path='/protected' component={Protected} />
+    </div>
+  </Router>,
   document.getElementById('root'));
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//       <AppRoutes />
+//   </BrowserRouter>,
+//   document.getElementById('root'));
+
 registerServiceWorker();

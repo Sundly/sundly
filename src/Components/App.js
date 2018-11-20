ec9c358174594dd49cd84c23a204bc779c3c1e12
Ben Orozco <benoror@gmail.com>
2018-11-19 21:53:47 -0600

    Ref[style]: Move Theme up to main index.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,32 +1,16 @@
 // Dependencies
 import React from "react";
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import PropTypes from 'prop-types';
 
 // Components
 import Shell from "./Shell";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#feea4e",
-      contrastText: "#444"
-    },
-    secondary: {
-      main: "#f44336",
-      contrastText: "#fff"
-    }
-  }
-});
 
 class App extends React.Component {
   render() {
     const {children} = this.props;
 
     return (
-      <MuiThemeProvider theme={theme}>
-        <Shell content={children} />
-      </MuiThemeProvider>
+      <Shell content={children} />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 // Blockstack app
 import App from './app'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#feea4e",
+      contrastText: "#444"
+    },
+    secondary: {
+      main: "#f44336",
+      contrastText: "#fff"
+    }
+  }
+});
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root'));
 
 registerServiceWorker();

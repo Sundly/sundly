2d5560c484550882379fc29166e5530c75deec69
Ben Orozco <benoror@gmail.com>
2018-12-16 12:01:41 -0600

    Feat[timeline]: Form for body measures & vital signs

--- src/Components/Timeline/index.js
+++ src/Components/Timeline/index.js
@@ -5,11 +5,21 @@ import { Link } from "react-router-dom";
 
 // Styles
 import { withStyles } from "@material-ui/core/styles";
+import classNames from 'classnames';
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import InputAdornment from '@material-ui/core/InputAdornment';
 import AddIcon from "@material-ui/icons/Add";
+import HeartIcon from "@material-ui/icons/FavoriteBorder";
+import WeightIcon from "@material-ui/icons/FitnessCenter";
+import HeightIcon from "@material-ui/icons/Straighten";
+import TempIcon from "@material-ui/icons/Whatshot";
+
+const blockstack = require('blockstack');
+
+const STORAGE_FILE = 'profile.json'
 
 const styles = theme => ({
   fab: {
@@ -47,11 +57,25 @@ const styles = theme => ({
 
 
 class TimeLine extends React.Component {
-  state = {
-    name: "Ben",
-    last_name: "Orozco",
-  };
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      user: blockstack.loadUserData(),
+      sundlyTimeline: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    const target = event.target
+
+    this.setState(prev => ({
+      ...prev,
+      [target.name]: target.value,
+    }))
+  }
 
   render() {
     const { classes } = this.props;
@@ -66,40 +90,92 @@ class TimeLine extends React.Component {
           </Grid>
           <Grid item xs={12}>
             <Paper className={classes.paper}>
-              07/13/2018
               <form className={classes.container} noValidate autoComplete="off">
                 <TextField
-                  disabled
-                  id="name"
-                  label="Name"
-                  className={classes.textField}
-                  value={this.state.name}
-                  margin="normal"
-                />
-                <TextField
-                  disabled
-                  id="last-name"
-                  label="Last Name"
-                  className={classes.textField}
-                  value={this.state.last_name}
-                  margin="normal"
-                />
-                <TextField
-                  id="full-width"
-                  label="Write Symptoms"
+                  id="datetime-local"
+                  className={classNames(classes.margin, classes.textField)}
+                  label="datetime"
+                  type="datetime-local"
+                  defaultValue={(new Date(Date.now())).toISOString()}
                   InputLabelProps={{
-                    shrink: true,
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <WeightIcon />
+                      </InputAdornment>
+                    )
                   }}
-                  placeholder="Symptoms"
-                  fullWidth
-                  multiline
-                  margin="normal"
+                />
+                <br/>
+                <TextField
+                  id="weight"
+                  className={classNames(classes.margin, classes.textField)}
+                  variant="outlined"
+                  label="Weight"
+                  value={this.state.weight}
+                  onChange={this.handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <WeightIcon />
+                      </InputAdornment>
+                    ),
+                    endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+                  }}
+                />
+                <br/>
+                <TextField
+                  id="height"
+                  className={classNames(classes.margin, classes.textField)}
+                  variant="outlined"
+                  label="Height"
+                  value={this.state.height}
+                  onChange={this.handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <HeightIcon />
+                      </InputAdornment>
+                    ),
+                    endAdornment: <InputAdornment position="end">Mts</InputAdornment>,
+                  }}
+                />
+                <br/>
+                <TextField
+                  id="temperature"
+                  className={classNames(classes.margin, classes.textField)}
+                  variant="outlined"
+                  label="Temperature"
+                  value={this.state.temperature}
+                  onChange={this.handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <TempIcon />
+                      </InputAdornment>
+                    ),
+                    endAdornment: <InputAdornment position="end">C</InputAdornment>,
+                  }}
+                />
+                <br/>
+                <TextField
+                  id="heart_rate"
+                  className={classNames(classes.margin, classes.textField)}
+                  variant="outlined"
+                  label="Heart Rate"
+                  value={this.state.heart_rate}
+                  onChange={this.handleChange}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <HeartIcon />
+                      </InputAdornment>
+                    ),
+                    endAdornment: <InputAdornment position="end">BPM</InputAdornment>,
+                  }}
                 />
               </form>
               <Button variant="fab" color={`primary`}>
-                <Link to="/">
-                  <AddIcon />
-                </Link>
+                <AddIcon />
               </Button>
             </Paper>
           </Grid>
